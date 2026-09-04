0e594c2c768cc9c16676cbfd864b9d351a671a01
Primenaty Chigorimbo <primenatychigorimbo1@gmail.com>
2026-09-04 15:42:03 +0200

    chore: add Spring Boot project setup

---
+++ src/main/java/com/kruskal1024/securetaskmanager/SecureTaskManagerApplication.java
@@ -0,0 +1,13 @@
+package com.kruskal1024.securetaskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureTaskManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureTaskManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kruskal1024/securetaskmanager/SecureTaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kruskal1024.securetaskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureTaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
